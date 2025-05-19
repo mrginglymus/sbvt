83fc7005cc7401e16e1b83b897a47c77517f007a
Bill Collins <bill@bill.works>
2025-05-19 18:22:20 +0100

    Oh dear

--- glb.js
+++ glb.js
@@ -1,8 +1,10 @@
-import {glob} from 'tinyglobby';
+import {glob as tinyglob} from 'tinyglobby';
 import fs from 'node:fs/promises';
+import {globby} from 'globby';
+import fastglob from 'fast-glob'
 
 const consume = async (gen) => {
-  const res =[];
+  const res = [];
   for await (const x of await gen) {
     res.push(x);
   }
@@ -11,8 +13,19 @@ const consume = async (gen) => {
 
 const patterns = ['../stories/**/*.jsx'];
 
-console.log(await glob(patterns, { cwd: './stories'}))
-console.log(await glob(patterns, { cwd: './.storybook'}))
 
+console.log('tinygloby');
+console.log(await tinyglob(patterns, {cwd: './stories'}))
+console.log(await tinyglob(patterns, {cwd: './.storybook'}))
+
+console.log('globby');
+console.log(await globby(patterns, {cwd: './stories'}))
+console.log(await globby(patterns, {cwd: './.storybook'}))
+
+console.log('fast-glob');
+console.log(await fastglob(patterns, {cwd: './stories'}))
+console.log(await fastglob(patterns, {cwd: './.storybook'}))
+
+console.log('native');
 console.log(await consume(fs.glob(patterns, {cwd: './stories'})))
 console.log(await consume(fs.glob(patterns, {cwd: './.storybook'})))
