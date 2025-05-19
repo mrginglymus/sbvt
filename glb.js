9ae32587560bb211567c7ddc41b726509ae7dd7e
Bill Collins <bill@bill.works>
2025-05-19 14:24:34 +0100

    Add stdlib glob for comparison

--- glb.js
+++ glb.js
@@ -1,4 +1,18 @@
 import {glob} from 'tinyglobby';
+import fs from 'node:fs/promises';
 
-console.log(await glob('../stories/**/*.jsx', { cwd: './stories'}))
-console.log(await glob('../stories/**/*.jsx', { cwd: './.storybook'}))
+const consume = async (gen) => {
+  const res =[];
+  for await (const x of await gen) {
+    res.push(x);
+  }
+  return res;
+}
+
+const patterns = ['../stories/**/*.jsx'];
+
+console.log(await glob(patterns, { cwd: './stories'}))
+console.log(await glob(patterns, { cwd: './.storybook'}))
+
+console.log(await consume(fs.glob(patterns, {cwd: './stories'})))
+console.log(await consume(fs.glob(patterns, {cwd: './.storybook'})))
